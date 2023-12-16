a4ca55d40cb9fbc24941bcedf6bc9290f7cedd30
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-16 10:26:37 -0300

    feat: add login functionality, update header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { LoginComponent } from "./pages/login/login.component";
 import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   { path: "", title: "Sentinel of Liberty", component: HomeComponent },
-  { path: "**", title: "404 - Not Found", component: NotFoundComponent },
+  { path: "login", title: "Login | SoL", component: LoginComponent },
+  { path: "terms", title: "Terms | SoL", component: TermsComponent },
+  { path: "privacy", title: "Privacy | SoL", component: PrivacyComponent },
+  { path: "**", title: "404 | SoL", component: NotFoundComponent },
 ];
