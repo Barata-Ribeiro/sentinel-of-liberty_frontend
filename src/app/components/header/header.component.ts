ac732d14a6c23ac41437f9be068258f0e963746d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-18 17:33:43 -0300

    refactor: adjust header component to include user authentication and logout functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { Observable, tap } from "rxjs";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-header",
@@ -7,4 +10,27 @@ import { Component } from "@angular/core";
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  userAuthenticated: boolean = false;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {
+    this.authService.isAuthenticated().subscribe(isAuthenticated => {
+      this.userAuthenticated = isAuthenticated;
+    });
+  }
+
+  logout(): Observable<string> {
+    return this.http
+      .get("http://localhost:3000/api/v1/auth/discord/logout", {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        responseType: "text",
+      })
+      .pipe(tap((message: string) => message));
+  }
+}
