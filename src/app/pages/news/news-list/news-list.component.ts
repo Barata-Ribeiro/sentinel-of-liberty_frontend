fa4e46e2ebdd3b32dd323b810f6e37ac0661532b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-24 06:55:13 -0300

    feat: add news-list and news-suggest components

---
+++ src/app/pages/news/news-list/news-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-list.component.html',
+  styleUrl: './news-list.component.css'
+})
+export class NewsListComponent {
+
+}
---
+++ src/app/pages/news/news-suggest/news-suggest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-suggest',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-suggest.component.html',
+  styleUrl: './news-suggest.component.css'
+})
+export class NewsSuggestComponent {
+
+}
