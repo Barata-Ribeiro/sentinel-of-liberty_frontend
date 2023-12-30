eb7cdc8c3887dadee033c5430b397a95f57c0a92
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-30 08:09:34 -0300

    refactor: add formatDate function and update articleData structure

--- src/app/pages/articles/article/article.component.ts
+++ src/app/pages/articles/article/article.component.ts
@@ -1,7 +1,9 @@
+import { formatDate } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { ActivatedRoute } from "@angular/router";
+import { IndividualArticleRequest } from "../../../@types/appTypes";
 
 @Component({
   selector: "app-article",
@@ -11,39 +13,74 @@ import { ActivatedRoute } from "@angular/router";
   styleUrl: "./article.component.css",
 })
 export class ArticleComponent implements OnInit, OnDestroy {
-  articleData: any;
+  articleData: IndividualArticleRequest;
 
   constructor(
     private readonly titleService: Title,
     private route: ActivatedRoute,
     private http: HttpClient
   ) {
-    this.articleData = {};
+    this.articleData = {
+      id: "",
+      title: "",
+      content: "",
+      contentSummary: "",
+      image: "",
+      createdAt: "",
+      updatedAt: "",
+      user: {
+        id: "",
+        discordUsername: "",
+        sol_username: "",
+        discordAvatar: "",
+      },
+      comments: [],
+    };
   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get("id");
-    if (id) {
-      this.retrieveArticle(id);
-    }
+    if (id) this.retrieveArticle(id);
   }
 
   ngOnDestroy(): void {
-    this.articleData = {};
+    this.articleData = {
+      id: "",
+      title: "",
+      content: "",
+      contentSummary: "",
+      image: "",
+      createdAt: "",
+      updatedAt: "",
+      user: {
+        id: "",
+        discordUsername: "",
+        sol_username: "",
+        discordAvatar: "",
+      },
+      comments: [],
+    };
   }
 
   private retrieveArticle(id: string): void {
     this.http
-      .get<any>(`http://localhost:3000/api/v1/articles/${id}`)
+      .get<IndividualArticleRequest>(
+        `http://localhost:3000/api/v1/articles/${id}`
+      )
       .subscribe({
         next: response => {
           this.articleData = {
             ...response,
+            createdAt: this.formatNewsDate(response.createdAt),
           };
           console.log(this.articleData);
           this.titleService.setTitle(`${this.articleData.title} | SoL`);
         },
         error: error => console.error(error),
       });
+  }
+
+  private formatNewsDate(dateString: string): string {
+    return formatDate(dateString, "MMMM dd, yyyy, h:mm a", "en-US");
   }
 }
