e2089c8cf0001b80cf23c4d5d96f5ef192d5947f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-31 07:55:59 -0300

    feat: update Angular configuration, add environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+const host = "http://localhost:3000";
+
+export const environment = {
+  production: false,
+  apiUrl: `${host}/api/v1`,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+const host = "https://example.com";
+
+export const environment = {
+  production: true,
+  apiUrl: `${host}/api/v1`,
+};
