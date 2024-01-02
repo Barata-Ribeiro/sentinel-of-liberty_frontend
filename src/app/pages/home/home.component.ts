ff29cd90206d36518bf4750bad89f41ab5bb8f8f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-02 08:59:45 -0300

    feat: adjust HomeComponent to include HTTP request and data formatting

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,58 @@
-import { Component } from "@angular/core";
+import { CommonModule, formatDate } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { environment } from "../../../environments/environment";
+import { HomeContentResponse } from "../../@types/appTypes";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  homeContentData: HomeContentResponse;
+
+  constructor(private http: HttpClient) {
+    this.homeContentData = {
+      articles: [],
+      suggestions: [],
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.homeContentData = {
+      articles: [],
+      suggestions: [],
+    };
+  }
+
+  ngOnInit(): void {
+    this.retrieveHomeData();
+  }
+
+  private retrieveHomeData(): void {
+    this.http.get<HomeContentResponse>(`${environment.apiUrl}/home`).subscribe({
+      next: response => {
+        this.homeContentData = {
+          ...response,
+          articles: response.articles.map(article => ({
+            ...article,
+            articleCreatedAt: this.formatDataDate(article.articleCreatedAt),
+          })),
+          suggestions: response.suggestions.map(suggestion => ({
+            ...suggestion,
+            createdAt: this.formatDataDate(suggestion.createdAt),
+          })),
+        };
+      },
+      error: error => console.error(error),
+    });
+  }
+
+  private formatDataDate(dateString: string): string {
+    return formatDate(dateString, "dd MMMM yyyy", "en-US");
+  }
+}
