9f8720b03121f5238a3ff0dac5aa41ca38fad71d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-05 06:44:43 -0300

    feat: add comment component, features still as TODO

--- src/app/components/comments/comment/comment.component.ts
+++ src/app/components/comments/comment/comment.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, Input, ViewChild } from "@angular/core";
+import { Comment } from "../../../@types/appTypes";
+import { CommentFormComponent } from "../comment-form/comment-form.component";
 
 @Component({
-  selector: 'app-comment',
+  selector: "app-comment",
   standalone: true,
-  imports: [],
-  templateUrl: './comment.component.html',
-  styleUrl: './comment.component.css'
+  imports: [CommonModule, CommentFormComponent],
+  templateUrl: "./comment.component.html",
+  styleUrl: "./comment.component.css",
 })
 export class CommentComponent {
+  @Input() comment!: Comment;
+  @Input() articleId: string;
+  replyMode = false;
+  editMode = false;
+  editedComment = "";
+  replies: Comment[] = [];
+  @ViewChild(CommentFormComponent) commentFormComponent!: CommentFormComponent;
 
+  constructor(private http: HttpClient) {
+    this.comment = {
+      id: "",
+      user: {
+        id: "",
+        discordUsername: "",
+        sol_username: "",
+      },
+      message: "",
+      likeCount: 0,
+      wasEdited: false,
+      createdAt: "",
+      updatedAt: "",
+    };
+
+    this.articleId = "";
+  }
+
+  toggleReplyMode() {
+    this.replyMode = !this.replyMode;
+    this.editMode = false;
+  }
+
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+    this.replyMode = false;
+    this.editedComment = this.comment.message;
+  }
+
+  toggleLike() {
+    // TODO: Implement toggle like
+  }
+
+  deleteComment() {
+    // TODO: Implement delete comment
+  }
+
+  postReply(message: string) {
+    // TODO: Implement post reply
+  }
+
+  updateComment() {
+    // TODO: Implement update comment
+  }
 }
