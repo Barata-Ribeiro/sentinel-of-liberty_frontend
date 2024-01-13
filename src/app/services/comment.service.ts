0a73a2f5c5be6e7e742cb6fba376129fc7b50184
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-13 09:28:15 -0300

    refactor: edit comment service with proper typing and use of injection

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,24 +1,25 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, catchError, of } from "rxjs";
 import { environment } from "../../environments/environment";
-import { ToggleLikeResponse } from "../@types/appTypes";
+import { Comment, ToggleLikeResponse } from "../@types/appTypes";
 
 @Injectable({
   providedIn: "root",
 })
 export class CommentService {
   private http = inject(HttpClient);
+  private headers = inject(HttpHeaders);
 
   postComment(
     articleId: string,
     commentData: { message: string; parentId?: string }
-  ) {
-    return this.http.post(
+  ): Observable<{ message: string; parentId?: string }> {
+    return this.http.post<{ message: string; parentId?: string }>(
       `${environment.apiUrl}/articles/${articleId}/comments`,
       commentData,
       {
-        headers: new HttpHeaders({ "Content-Type": "application/json" }),
+        headers: this.headers.set("Content-Type", "application/json"),
         withCredentials: true,
       }
     );
@@ -28,22 +29,36 @@ export class CommentService {
     articleId: string,
     commentId: string,
     commentData: { message: string }
-  ) {
-    return this.http.put(
-      `${environment.apiUrl}/articles/${articleId}/comments/${commentId}`,
-      commentData,
-      {
-        headers: new HttpHeaders({ "Content-Type": "application/json" }),
-        withCredentials: true,
-      }
-    );
+  ): Observable<{ message: string }> {
+    return this.http
+      .put<{ message: string }>(
+        `${environment.apiUrl}/articles/${articleId}/comments/${commentId}`,
+        commentData,
+        {
+          headers: this.headers.set("Content-Type", "application/json"),
+          withCredentials: true,
+        }
+      )
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          return of({} as { message: string });
+        })
+      );
   }
 
-  deleteComment(articleId: string, commentId: string) {
-    return this.http.delete(
-      `${environment.apiUrl}/articles/${articleId}/comments/${commentId}`,
-      { withCredentials: true }
-    );
+  deleteComment(articleId: string, commentId: string): Observable<Comment> {
+    return this.http
+      .delete<Comment>(
+        `${environment.apiUrl}/articles/${articleId}/comments/${commentId}`,
+        { withCredentials: true }
+      )
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          return of({} as Comment);
+        })
+      );
   }
 
   toggleLike(
@@ -54,6 +69,11 @@ export class CommentService {
       `${environment.apiUrl}/articles/${articleId}/comments/${commentId}/likes`,
       {},
       { withCredentials: true }
+    ).pipe(
+      catchError(error => {
+        console.error(error);
+        return of({} as ToggleLikeResponse);
+      })
     );
   }
 }
