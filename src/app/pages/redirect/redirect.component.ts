7d92bff53375c24eb934a928f2bbc0819dd17c42
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-17 08:47:19 -0300

    refactor: adjust redirect component and fix subscription issue

--- src/app/pages/redirect/redirect.component.ts
+++ src/app/pages/redirect/redirect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from "@angular/core";
+import { Component, OnDestroy, inject } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { Subscription } from "rxjs";
@@ -13,102 +13,102 @@ import { CookieService } from "../../services/cookie.service";
   templateUrl: "./redirect.component.html",
   styleUrl: "./redirect.component.css",
 })
-export class RedirectComponent implements OnInit, OnDestroy {
+export class RedirectComponent implements OnDestroy {
   private authService = inject(AuthService);
   private cookieService = inject(CookieService);
   private toastrService = inject(ToastrService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  private subscriptions = new Subscription();
+
+  private subscription = new Subscription();
 
   protected userId: string = "";
 
-  ngOnInit(): void {
-    const queryParamsSubscription = this.route.queryParams.subscribe(params => {
-      const code = params["code"];
-      if (code) {
-        this.loginWithDiscord(code);
-      } else this.router.navigate(["/login"]);
-    });
-
-    this.subscriptions.add(queryParamsSubscription);
+  constructor() {
+    this.subscription.add(
+      this.route.queryParams.subscribe(params => {
+        const code = params["code"];
+        code ? this.loginWithDiscord(code) : this.router.navigate(["/login"]);
+      })
+    );
   }
 
   ngOnDestroy(): void {
     this.userId = "";
-    this.subscriptions.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   private loginWithDiscord(code: string) {
-    return this.subscriptions.add(
-      this.authService.loginWithDiscord(code).subscribe({
-        next: response => {
-          this.cookieService.setCookie(
-            "authToken",
-            response.authToken,
-            1,
-            true,
-            "None"
-          );
+    return this.authService.loginWithDiscord(code).subscribe({
+      next: response => {
+        this.cookieService.setCookie(
+          "authToken",
+          response.authToken,
+          1,
+          true,
+          "None"
+        );
 
-          this.cookieService.setCookie("userId", response.id, 1, true, "None");
+        this.cookieService.setCookie("userId", response.id, 1, true, "None");
 
-          this.cookieService.setCookie(
-            "refreshToken",
-            response.refreshToken,
-            365,
-            true,
-            "None"
-          );
+        this.cookieService.setCookie(
+          "refreshToken",
+          response.refreshToken,
+          365,
+          true,
+          "None"
+        );
 
-          this.userId = response.id;
-          this.retrieveUserData(this.userId);
-          this.toastrService.show(response.message, "Success!", {
-            toastComponent: CustomToastrComponent,
-            toastClass:
-              "shadow-[5px_5px_0px_0px_rgba(217,249,157)] max-w-sm rounded-lg border border-lime-200 bg-lime-100 dark:border-lime-900 dark:bg-lime-800/10 dark:text-lime-500",
-            titleClass: "text-lime-800 font-bold text-lg",
-            messageClass: "text-lime-800 font-medium text-normal",
-          });
-          this.router.navigate(["/profile"]);
-        },
-        error: error => {
-          this.toastrService.show(error.message, "Error!", {
-            toastComponent: CustomToastrComponent,
-            toastClass:
-              "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
-            titleClass: "text-red-800 font-bold text-lg",
-            messageClass: "text-red-800 font-medium text-normal",
-          });
-          this.router.navigate(["/login"]);
-        },
-      })
-    );
+        this.userId = response.id;
+
+        this.retrieveUserData(this.userId);
+
+        this.toastrService.show(response.message, "Success!", {
+          toastComponent: CustomToastrComponent,
+          toastClass:
+            "shadow-[5px_5px_0px_0px_rgba(217,249,157)] max-w-sm rounded-lg border border-lime-200 bg-lime-100 dark:border-lime-900 dark:bg-lime-800/10 dark:text-lime-500",
+          titleClass: "text-lime-800 font-bold text-lg",
+          messageClass: "text-lime-800 font-medium text-normal",
+        });
+
+        this.router.navigate(["/profile"]);
+      },
+      error: error => {
+        this.toastrService.show(error.message, "Error!", {
+          toastComponent: CustomToastrComponent,
+          toastClass:
+            "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
+          titleClass: "text-red-800 font-bold text-lg",
+          messageClass: "text-red-800 font-medium text-normal",
+        });
+
+        this.router.navigate(["/login"]);
+      },
+    });
   }
 
   private retrieveUserData(id: string) {
-    return this.subscriptions.add(
-      this.authService.getUserById(id).subscribe({
-        next: response => {
-          this.cookieService.setCookie(
-            "userData",
-            JSON.stringify(response),
-            1,
-            true,
-            "None"
-          );
-        },
-        error: error => {
-          this.toastrService.show(error.message, "Error!", {
-            toastComponent: CustomToastrComponent,
-            toastClass:
-              "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
-            titleClass: "text-red-800 font-bold text-lg",
-            messageClass: "text-red-800 font-medium text-normal",
-          });
-          this.router.navigate(["/login"]);
-        },
-      })
-    );
+    return this.authService.getUserById(id).subscribe({
+      next: response => {
+        this.cookieService.setCookie(
+          "userData",
+          JSON.stringify(response),
+          1,
+          true,
+          "None"
+        );
+      },
+      error: error => {
+        this.toastrService.show(error.message, "Error!", {
+          toastComponent: CustomToastrComponent,
+          toastClass:
+            "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
+          titleClass: "text-red-800 font-bold text-lg",
+          messageClass: "text-red-800 font-medium text-normal",
+        });
+        
+        this.router.navigate(["/login"]);
+      },
+    });
   }
 }
