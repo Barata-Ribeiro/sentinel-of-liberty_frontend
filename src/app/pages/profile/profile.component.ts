16469a6cb27451a941b94641f1fb9f0dd0f72573
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-18 06:30:08 -0300

    feat: add ToastrService and Subscription imports and handle error messages

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,10 +2,13 @@ import { CommonModule, DatePipe } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { Router } from "express";
+import { ToastrService } from "ngx-toastr";
+import { Subscription } from "rxjs";
 import { environment } from "../../../environments/environment";
 import { EditDataRequest, User } from "../../@types/appTypes";
 import { DeleteAccountModalComponent } from "../../components/delete-account-modal/delete-account-modal.component";
 import { EditAccountModalComponent } from "../../components/edit-account-modal/edit-account-modal.component";
+import { CustomToastrComponent } from "../../components/shared/custom-toastr/custom-toastr.component";
 import { CookieService } from "../../services/cookie.service";
 
 @Component({
@@ -22,12 +25,14 @@ import { CookieService } from "../../services/cookie.service";
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   private cookieService = inject(CookieService);
+  private toastrService = inject(ToastrService);
   private http = inject(HttpClient);
   private router = inject(Router);
+  private subscriptions = new Subscription();
 
-  user: User | null = null;
-  showModalDelete = false;
-  showEditProfile = false;
+  protected user: User | null = null;
+  protected showModalDelete = false;
+  protected showEditProfile = false;
 
   ngOnInit(): void {
     this.loadUser();
@@ -35,6 +40,7 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.user = null;
+    this.subscriptions.unsubscribe();
   }
 
   // Delete account modal
@@ -47,23 +53,31 @@ export class ProfileComponent implements OnInit, OnDestroy {
   }
 
   handleDeactivate() {
-    try {
+    return this.subscriptions.add(
       this.http
         .delete(`${environment.apiUrl}/users/${this.user?.id}`, {
           withCredentials: true,
         })
         .subscribe({
           next: () => {
+            this.closeModalDelete();
             this.cookieService.deleteCookie("userId");
             this.cookieService.deleteCookie("userData");
             this.router.navigate(["/login"]);
           },
-          error: () => this.router.navigate(["/login"]),
-        });
-    } catch (error) {
-      console.error(error);
-    }
-    this.closeModalDelete();
+          error: error => {
+            this.closeModalDelete();
+            this.toastrService.show(error.message, "Error!", {
+              toastComponent: CustomToastrComponent,
+              toastClass:
+                "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
+              titleClass: "text-red-800 font-bold text-lg",
+              messageClass: "text-red-800 font-medium text-normal",
+            });
+            this.router.navigate(["/login"]);
+          },
+        })
+    );
   }
 
   // Edit profile modal
@@ -76,7 +90,7 @@ export class ProfileComponent implements OnInit, OnDestroy {
   }
 
   handleEditAccount(editData: EditDataRequest) {
-    try {
+    return this.subscriptions.add(
       this.http
         .put(`${environment.apiUrl}/users/${this.user?.id}`, editData, {
           withCredentials: true,
@@ -86,11 +100,16 @@ export class ProfileComponent implements OnInit, OnDestroy {
             this.fetchUserFromAuthToken();
             this.closeEditModal();
           },
-          error: error => alert("Error: " + error.message),
-        });
-    } catch (error) {
-      console.error(error);
-    }
+          error: error =>
+            this.toastrService.show(error.message, "Error!", {
+              toastComponent: CustomToastrComponent,
+              toastClass:
+                "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
+              titleClass: "text-red-800 font-bold text-lg",
+              messageClass: "text-red-800 font-medium text-normal",
+            }),
+        })
+    );
   }
 
   private loadUser(): void {
@@ -107,23 +126,34 @@ export class ProfileComponent implements OnInit, OnDestroy {
   private fetchUserFromAuthToken(): void {
     const userId = this.cookieService.getCookieString("userId");
     if (userId) {
-      this.http
-        .get<User>(`${environment.apiUrl}/users/${userId}`, {
-          withCredentials: true,
-        })
-        .subscribe({
-          next: response => {
-            this.user = { ...response };
-            this.cookieService.setCookie(
-              "userData",
-              JSON.stringify(response),
-              1,
-              true,
-              "None"
-            );
-          },
-          error: () => this.router.navigate(["/login"]),
-        });
+      return this.subscriptions.add(
+        this.http
+          .get<User>(`${environment.apiUrl}/users/${userId}`, {
+            withCredentials: true,
+          })
+          .subscribe({
+            next: response => {
+              this.user = { ...response };
+              this.cookieService.setCookie(
+                "userData",
+                JSON.stringify(response),
+                1,
+                true,
+                "None"
+              );
+            },
+            error: error => {
+              this.toastrService.show(error.message, "Error!", {
+                toastComponent: CustomToastrComponent,
+                toastClass:
+                  "max-w-xs rounded-lg border border-red-200 bg-red-100 text-sm text-red-800 shadow-[5px_5px_0px_0px_rgba(254,202,202)] dark:border-red-900 dark:bg-red-800/10 dark:text-red-500",
+                titleClass: "text-red-800 font-bold text-lg",
+                messageClass: "text-red-800 font-medium text-normal",
+              }),
+                this.router.navigate(["/login"]);
+            },
+          })
+      );
     }
   }
 }
