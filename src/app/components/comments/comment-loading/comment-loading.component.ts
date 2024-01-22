fc295f848b708e40d2dde1f2388b240884c65bfd
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-22 10:53:00 -0300

    feat: add comment loading component

---
+++ src/app/components/comments/comment-loading/comment-loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-loading',
+  standalone: true,
+  imports: [],
+  templateUrl: './comment-loading.component.html',
+  styleUrl: './comment-loading.component.css'
+})
+export class CommentLoadingComponent {
+
+}
