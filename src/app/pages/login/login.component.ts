cfe7d451722b19e320a19a10e766ad308a516f19
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-22 06:13:35 -0300

    refactor: change href to router links in login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { environment } from "../../../environments/environment";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-login",
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
