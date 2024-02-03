7a23b8220b433dfddba8cfe50ad9a8a5474e8d93
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-03 07:27:50 -0300

    feat: add in-memory scrolling to router configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withInMemoryScrolling } from "@angular/router";
 
 import {
   provideHttpClient,
@@ -17,7 +17,13 @@ import { authInterceptor } from "./auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: "top",
+        anchorScrolling: "enabled",
+      })
+    ),
     provideAnimations(),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideClientHydration(
